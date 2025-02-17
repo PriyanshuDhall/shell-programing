38dfe430cb1ccf931ecf57636a54d6c06810a8b4
PriyanshuDhall <priyanshudhall2002@gmail.com>
2025-02-17 16:06:58 +0530

    palindrome.sh

---
+++ palindrome.sh
@@ -0,0 +1,14 @@
+A shell program to find whether a number is palindrome or not.
+#! /bin/bash
+# Priyanshu Dhall , 24mca022
+echo "Enter a number:"
+read num
+rev_num=$ (echo $num | rev)
+if [
+"$num"
+echo
+=
+"$rev _num" ]; then
+"'num is a Palindrome"
+else
+echo "$num is Not a Palindrome"
