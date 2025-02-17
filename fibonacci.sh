79caf5faca6e571dcdee80640210f33d2951597e
PriyanshuDhall <priyanshudhall2002@gmail.com>
2025-02-17 16:03:16 +0530

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,10 @@
+a shell program to find Fibonacci series.
+#! /bin/bash
+# Priyanshu Dhall , 24mca022
+echo -n "Enter the number of terms:
+read n
+a=0
+b=1 echo "Fibonacci Series:"
+echo -n "Sa $b for (( 1=2; i‹n; i# ))
+do c=$(a + b)) echo an "$c a=$b
+b=$c
